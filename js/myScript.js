46cfc1dd8b3f593c45cd1f735473bf1c73a4508a
James H <hernandez.jimmy@gmail.com>
2019-12-03 22:27:57 -0600

    Added Kyla's dropdown li to the laptop product overview page and added animations to fas icon

---
+++ js/myScript.js
@@ -0,0 +1,51 @@
+// js for product overview laptop dropdown menus
+
+   document.getElementById('the-button-1').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-2').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-3').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-4').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-5').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-6').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-7').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-8').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-9').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
+
+  document.getElementById('the-button-10').addEventListener('click', function() {
+    var icon = this.querySelector('.right i.fas');
+    icon.classList.toggle('rotate');
+  });
